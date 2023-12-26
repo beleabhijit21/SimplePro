6353f93163050c70c09c7840c472da0432a78718
beleabhijit21 <beleabhijit21@gmail.com>
2023-12-26 12:38:53 +0530

    node js project by chatgpt

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, CI/CD World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
